396c731f480b27159f4a70b0bd98fe4c7f3dbdad
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2017-07-03 11:02:41 +0200

    Timber/twig improvments

--- page.php
+++ page.php
@@ -1,13 +1,6 @@
 <?php
 
 $context = Timber::get_context();
-$context['post'] = new TimberPost();
+$context['page'] = new TimberPost();
 
-// Look for a custom template
-if($slug = get_page_template_slug($post->ID) != '') {
-  $template = "page-$slug.twig";
-} else {
-  $template = 'page.twig';
-}
-
-Timber::render($template, $context);
+Timber::render(array($context['page']->post_name.'.twig', 'page.twig'), $context);
